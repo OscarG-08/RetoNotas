35ece850ff460e6df8dd6f2307feb6a0623e5f5d
OscarG-08 <oscarg-ricardos@hotmail.com>
2023-12-28 10:53:53 -0500

    Se creo una pagina paracalcular el promeido y mostrar un gif de exito o fracaso segun su promedio

---
+++ promedioNotas.js
@@ -0,0 +1,12 @@
+calcularPromedioNotas = function(){
+    let nota1 = recuperarFlotante("txtNota1");
+    let nota2 = recuperarFlotante("txtNota2");
+    let nota3 = recuperarFlotante("txtNota3");
+    let promedio = calcularPromedio(nota1, nota2, nota3);
+    cambiarTexto("lblPromedio", promedio.toFixed(2));
+    if (promedio > 7) {
+        cambiarImagen("imagen", "exito.gif");
+    } else {
+        cambiarImagen("imagen", "fracaso.gif");
+    }
+}
